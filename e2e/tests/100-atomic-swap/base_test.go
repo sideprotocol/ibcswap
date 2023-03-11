5fd98a685b96547df34c2ca60f51a9b7a27ff4e1
Emil Georgiev <emogeorgiev88@gmail.com>
2023-03-11 10:41:14 +0200

    assert balances

--- e2e/tests/100-atomic-swap/base_test.go
+++ e2e/tests/100-atomic-swap/base_test.go
@@ -34,22 +34,50 @@ func (s *AtomicSwapTestSuite) TestMakeSwap() {
 	chainA, chainB := s.GetChains()
 
 	chainADenom := chainA.Config().Denom
-	//chainBDenom := chainB.Config().Denom
+	chainBDenom := chainB.Config().Denom
 
-	// // create wallets for testing
-	chainAWallet := s.CreateUserOnChainA(ctx, testvalues.StartingTokenAmount)
-	senderAddress := chainAWallet.Bech32Address("cosmos")
-	chainBWallet := s.CreateUserOnChainB(ctx, testvalues.StartingTokenAmount)
-	senderReceivingAddress := chainBWallet.Bech32Address("cosmos")
+	fmt.Println("-----------------------------------------------------------")
+	// create wallets for testing of the maker address on chains A and B.
+	chainAMakerWallet := s.CreateUserOnChainA(ctx, testvalues.StartingTokenAmount)
+	makerAddressOnChainA := chainAMakerWallet.Bech32Address("cosmos")
+	fmt.Println(makerAddressOnChainA)
+	chainBMakerWallet := s.CreateUserOnChainB(ctx, testvalues.StartingTokenAmount)
+	makerReceivingAddressOnChainB := chainBMakerWallet.Bech32Address("cosmos")
+	fmt.Println(makerReceivingAddressOnChainB)
+	fmt.Println()
+	// create wallets for testing of the taker address on chains A and B.
+	chainBTakerWallet := s.CreateUserOnChainB(ctx, testvalues.StartingTokenAmount)
+	takerAddressOnChainB := chainBTakerWallet.Bech32Address("cosmos")
+	fmt.Println(takerAddressOnChainB)
+	chainATakerWallet := s.CreateUserOnChainA(ctx, testvalues.StartingTokenAmount)
+	takerReceivingAddressOnChainA := chainATakerWallet.Bech32Address("cosmos")
+	fmt.Println(takerReceivingAddressOnChainA)
+	fmt.Println("-----------------------------------------------------------")
 
 	// allocate tokens to the new account
-	initialBalances := sdk.NewCoins(sdk.NewCoin(chainADenom, sdk.NewInt(10000000000)))
-	err := s.SendCoinsFromModuleToAccount(ctx, chainA, chainAWallet, initialBalances)
+	//initialBalances := sdk.NewCoins(sdk.NewCoin(chainADenom, sdk.NewInt(10000000000)))
+	//err := s.SendCoinsFromModuleToAccount(ctx, chainA, chainAMakerWallet, initialBalances)
 
+	//s.Require().NoError(err)
+	res1, err := s.QueryBalance(ctx, chainA, makerAddressOnChainA, chainADenom)
 	s.Require().NoError(err)
-	res, err := s.QueryBalance(ctx, chainA, senderAddress, chainADenom)
-	s.Require().NotEqual(res.Balance.Amount, sdk.NewInt(0))
+	makerInitialBallanceOnChainA := res1.Balance.Amount
+	fmt.Println("Initial balance 1:", makerInitialBallanceOnChainA)
+
+	res2, err := s.QueryBalance(ctx, chainB, makerReceivingAddressOnChainB, chainBDenom)
 	s.Require().NoError(err)
+	makerInitialBallanceOnChainB := res2.Balance.Amount
+	fmt.Println("Initial balance 2: ", makerInitialBallanceOnChainB)
+
+	res3, err := s.QueryBalance(ctx, chainA, takerReceivingAddressOnChainA, chainADenom)
+	s.Require().NoError(err)
+	takerInitialBallanceOnChainA := res3.Balance.Amount
+	fmt.Println("Initial balance 3:", takerInitialBallanceOnChainA)
+
+	res4, err := s.QueryBalance(ctx, chainB, takerAddressOnChainB, chainBDenom)
+	s.Require().NoError(err)
+	takerInitialBallanceOnChainB := res4.Balance.Amount
+	fmt.Println("Initial balance 4:", takerInitialBallanceOnChainB)
 
 	t.Run("start relayer", func(t *testing.T) {
 		s.StartRelayer(relayer)
@@ -64,15 +92,15 @@ func (s *AtomicSwapTestSuite) TestMakeSwap() {
 			channelA.ChannelID,
 			sellToken,
 			buyToken,
-			senderAddress,
-			senderReceivingAddress,
+			makerAddressOnChainA,
+			makerReceivingAddressOnChainB,
 			"",
 			timeoutHeight,
 			0,
 			time.Now().UTC().Unix(),
 		)
 
-		resp, err := s.BroadcastMessages(ctx, chainA, chainAWallet, msg)
+		resp, err := s.BroadcastMessages(ctx, chainA, chainAMakerWallet, msg)
 		s.AssertValidTxResponse(resp)
 		s.Require().NoError(err)
 
@@ -82,8 +110,6 @@ func (s *AtomicSwapTestSuite) TestMakeSwap() {
 		// TAKE SWAP
 
 		sellToken2 := sdk.NewCoin(chainB.Config().Denom, sdk.NewInt(50))
-		senderAddressB := chainBWallet.Bech32Address("cosmos")
-		senderReceivingAddressA := chainAWallet.Bech32Address("cosmos")
 
 		timeoutHeight2 := clienttypes.NewHeight(0, 110)
 		order := types.NewAtomicOrder(types.NewMakerFromMsg(msg), msg.SourceChannel)
@@ -91,16 +117,24 @@ func (s *AtomicSwapTestSuite) TestMakeSwap() {
 			channelA.PortID,
 			channelA.ChannelID,
 			sellToken2,
-			senderAddressB,
-			senderReceivingAddressA,
+			takerAddressOnChainB,
+			takerReceivingAddressOnChainA,
 			timeoutHeight2,
 			0,
 			time.Now().UTC().Unix(),
 		)
 		msgTake.OrderId = order.Id
 
-		resp2, err2 := s.BroadcastMessages(ctx, chainB, chainBWallet, msgTake)
-
+		resp2, err2 := s.BroadcastMessages(ctx, chainB, chainBTakerWallet, msgTake)
+		fmt.Println("-----------------------------")
+		fmt.Println("-----------------------------")
+		fmt.Println("-----------------------------")
+		fmt.Println("-----------------------------")
+		fmt.Println(resp2)
+		fmt.Println("-----------------------------")
+		fmt.Println("-----------------------------")
+		fmt.Println("-----------------------------")
+		fmt.Println("-----------------------------")
 		s.AssertValidTxResponse(resp2)
 		s.Require().NoError(err2)
 
@@ -110,46 +144,45 @@ func (s *AtomicSwapTestSuite) TestMakeSwap() {
 		// check packet relay status.
 		//s.AssertPacketRelayed(ctx, chainB, channelA.PortID, channelA.ChannelID, 1)
 
-		b1, err := s.QueryBalance(ctx, chainA, senderAddress, chainA.Config().Denom)
+		b1, err := s.QueryBalance(ctx, chainA, makerAddressOnChainA, chainA.Config().Denom)
 		s.Require().NoError(err)
 		fmt.Println("FINALLLLL 1")
 		fmt.Println(b1.Balance.String())
 
-		b2, err := s.QueryBalance(ctx, chainA, senderReceivingAddressA, chainA.Config().Denom)
+		b2, err := s.QueryBalance(ctx, chainB, makerReceivingAddressOnChainB, chainB.Config().Denom)
 		s.Require().NoError(err)
 		fmt.Println("FINALLLLL 2")
 		fmt.Println(b2.Balance.String())
 
-		b3, err := s.QueryBalance(ctx, chainB, senderAddressB, chainB.Config().Denom)
+		b3, err := s.QueryBalance(ctx, chainA, takerReceivingAddressOnChainA, chainA.Config().Denom)
 		s.Require().NoError(err)
 		fmt.Println("FINALLLLL 3")
 		fmt.Println(b3.Balance.String())
 
-		b4, err := s.QueryBalance(ctx, chainB, senderReceivingAddress, chainB.Config().Denom)
+		b4, err := s.QueryBalance(ctx, chainB, takerAddressOnChainB, chainB.Config().Denom)
 		s.Require().NoError(err)
 		fmt.Println("FINALLLLL 4")
 		fmt.Println(b4.Balance.String())
-
 	})
 
 	//t.Run("send take swap message", func(t *testing.T) {
 	//	sellToken := sdk.NewCoin(chainB.Config().Denom, sdk.NewInt(50))
-	//	senderAddressB := chainBWallet.Bech32Address("cosmos")
-	//	senderReceivingAddressA := chainAWallet.Bech32Address("CosmosA")
+	//	takerAddressOnChainB := chainBMakerWallet.Bech32Address("cosmos")
+	//	takerReceivingAddressOnChainA := chainAMakerWallet.Bech32Address("CosmosA")
 	//
 	//	timeoutHeight := clienttypes.NewHeight(0, 110)
 	//	msgTake := types.NewMsgTakeSwap(
 	//		channelA.PortID,
 	//		channelA.ChannelID,
 	//		sellToken,
-	//		senderAddressB,
-	//		senderReceivingAddressA,
+	//		takerAddressOnChainB,
+	//		takerReceivingAddressOnChainA,
 	//		timeoutHeight,
 	//		0,
 	//		time.Now().UTC().Unix(),
 	//	)
 	//
-	//	resp, err := s.BroadcastMessages(ctx, chainB, chainBWallet, msgTake)
+	//	resp, err := s.BroadcastMessages(ctx, chainB, chainBMakerWallet, msgTake)
 	//	fmt.Println("-------------------")
 	//	fmt.Println("-------------------")
 	//	fmt.Println("-------------------")
