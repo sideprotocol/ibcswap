1ee5e66921bea9432ee71e261b370b5532f0642f
Emil Georgiev <emogeorgiev88@gmail.com>
2023-03-15 23:08:48 +0200

    add e2e test case make swap when maker doesn't have enough tokens

--- e2e/tests/100-atomic-swap/base_test.go
+++ e2e/tests/100-atomic-swap/base_test.go
@@ -107,24 +107,16 @@ func (s *AtomicSwapTestSuite) TestAtomicSwap_HappyPath() {
 		s.Require().NoError(err)
 		s.Require().Equal(takerInitialBallanceOnChainB.Int64()-50, b4.Balance.Amount.Int64())
 	})
-}
 
-func (s *AtomicSwapTestSuite) TestCancelAtomicSwap_HappyPath() {
-	t := s.T()
-	ctx := context.TODO()
-
-	// setup relayers and connection-0 between two chains.
-	relayer, channelA := s.SetupChainsRelayerAndChannel(ctx, atomicSwapChannelOptions())
-	chainA, chainB := s.GetChains()
-
-	// create wallets for testing of the maker address on chains A and B.
-	chainAMakerWallet := s.CreateUserOnChainA(ctx, testvalues.StartingTokenAmount)
-	makerAddressOnChainA := chainAMakerWallet.Bech32Address("cosmos")
-	chainBMakerWallet := s.CreateUserOnChainB(ctx, testvalues.StartingTokenAmount)
-	makerReceivingAddressOnChainB := chainBMakerWallet.Bech32Address("cosmos")
-
-	t.Run("start relayer", func(t *testing.T) {
-		s.StartRelayer(relayer)
+	t.Run("maker try to sell more tokens then he has in his account", func(t *testing.T) {
+		// Broadcast Make Swap transaction.
+		sellToken := sdk.NewCoin(chainA.Config().Denom, sdk.NewInt(200_000_000))
+		buyToken := sdk.NewCoin(chainB.Config().Denom, sdk.NewInt(50))
+		timeoutHeight := clienttypes.NewHeight(0, 110)
+		msg := types.NewMsgMakeSwap(channelA.PortID, channelA.ChannelID, sellToken, buyToken, makerAddressOnChainA, makerReceivingAddressOnChainB, "", timeoutHeight, 0, time.Now().UTC().Unix())
+		resp, err := s.BroadcastMessages(ctx, chainA, chainAMakerWallet, msg)
+		s.Require().NoError(err)
+		s.Equal("failed to execute message; message index: 0: insufficient balance", resp.RawLog)
 	})
 
 	t.Run("cancel atomic swap", func(t *testing.T) {
